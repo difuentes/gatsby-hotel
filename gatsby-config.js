cc9b8c85c28999a49309d136deb2c2ef40b66ba6
difuentes <fuentesdiego054@gmail.com>
2020-09-19 10:27:34 -0300

    Pagina de inicio, Pagina nosotros y conexion a DatosCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+
     'gatsby-plugin-emotion',
     `gatsby-plugin-react-helmet`,
     {
@@ -28,8 +29,11 @@ module.exports = {
         //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+        resolve: 'gatsby-source-datocms',
+        options:{
+          apiToken: '534b57d9ec2d5a06c8216d77ccd2e1'
+        }
+    }
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = (props) => {
             <Helmet>
                 <title>DiFuentes - Gastby Hotel</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+                <link  href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
             </Helmet>
             <Header/>
             {props.children}
